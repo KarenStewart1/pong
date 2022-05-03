594523e8b658912c4bd571e989e38be60cce0652
Karen Stewart <88774920+KarenStewart1@users.noreply.github.com>
2022-05-03 19:30:09 +0200

    upload all files to github

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const io = require("socket.io");
+
+const apiServer = require("./api");
+const httpServer = http.createServer(apiServer);
+const sockets = require("./sockets");
+const socketServer = io(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = 3000;
+
+httpServer.listen(PORT);
+// console.log(`listening on port ${PORT}`);
+
+sockets.listen(socketServer);
